3a518b3d8996b3794e1325ebff0a9b6cade6b053
Cédric Fressin <hello@cedricfressin.com>
2023-12-08 10:45:18 +0100

    chore: bootstrap app package

---
+++ packages/mobile/App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar'
+import { ScrollView } from 'react-native'
+
+export default function App() {
+  return (
+    <ScrollView>
+      <StatusBar style="auto" />
+    </ScrollView>
+  )
+}
